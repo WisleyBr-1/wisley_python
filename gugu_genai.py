5f3a2d30f6e56687884a84b13246010690f2e592
WisleyBr-1 <wisleyemanuelgv45@gmail.com>
2026-04-25 15:56:53 -0300

    Removendo chave de API gugu_genai.py

--- gugu_genai.py
+++ gugu_genai.py
@@ -1,7 +1,7 @@
 from google import genai
 
 # The client gets the API key from the environment variable `GEMINI_API_KEY`.
-client = genai.Client(api_key= "AIzaSyAg14EY6zj2-_XMj5ed3GEZWXyztX32vdM")
+client = genai.Client(api_key= "AIzaSy...")
 
 resposta = client.models.generate_content(
     model="gemini-3-flash-preview", 
